4f3a0f3ec73e9ee87f2fb00f1c738c9cdf13bbb2
akuk <amjadkhamuk1975@gmail.com>
2023-05-09 01:57:53 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,44 +1,55 @@
-import React, { useState,useEffect } from 'react'
-import './App.css';
-import Axios from 'axios'
-import Dropdown from 'react-dropdown';
-import 'react-dropdown/style.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Axios from "axios";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
 
 function App() {
-  const [data, setData] = useState({})
-  const [location, setLocation] = useState('')
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("chennai");
   const [options, setOptions] = useState([]);
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=9f3442e129fd928d90ac0f7f3478311e`
-  
-  
-    
-  const cities=["bhopal","Bangalore","chennai","kolkata","delhi","hyderabad","jaipur","kanpur","kochi","Mumbai"]
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=9f3442e129fd928d90ac0f7f3478311e`;
 
-   function searchLocation ()
-    {
-      Axios.get(url).then((response) => {
-        setData(response.data)
-        console.log(response.data)
-      })
-      
-    }
-  
+  const cities = [
+    "bhopal",
+    "Bangalore",
+    "chennai",
+    "kolkata",
+    "delhi",
+    "hyderabad",
+    "jaipur",
+    "kanpur",
+    "kochi",
+    "Mumbai",
+  ];
+
+  //  function searchLocation ()
+  //   {
+  //     Axios.get(url).then((response) => {
+  //       setData(response.data)
+  //       console.log(response.data)
+  //     })
+
+  //   }
+
+  useEffect(() => {
+    Axios.get(url).then((res) => {
+      setData(res.data);
+    });
+  }, [location]);
 
   return (
     <div className="app">
       <div className="search">
-   
-            <Dropdown
-                options={cities}
-                onChange ={(e) => {
-                  setLocation(e.value)}
-                  
-              }
-                
-                placeholder="Location"
-              />  
-              <button onClick={searchLocation}>view weather</button>
+        <Dropdown
+          options={cities}
+          onChange={(e) => {
+            setLocation(e.value);
+          }}
+          placeholder="Chennai"
+        />
+        {/* <button onClick={searchLocation}>view weather</button> */}
       </div>
       <div className="container">
         <div className="top">
@@ -53,25 +64,26 @@ function App() {
           </div>
         </div>
 
-        {data.name !== undefined &&
+        {data.name !== undefined && (
           <div className="bottom">
             <div className="feels">
-              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              {data.main ? (
+                <p className="bold">{data.main.feels_like.toFixed()}°F</p>
+              ) : null}
               <p>Feels Like</p>
             </div>
             <div className="humidity">
-              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
               <p>Humidity</p>
             </div>
             <div className="wind">
-              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              {data.wind ? (
+                <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+              ) : null}
               <p>Wind Speed</p>
             </div>
           </div>
-        }
-
-
-
+        )}
       </div>
     </div>
   );
